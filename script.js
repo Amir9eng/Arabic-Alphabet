ecb396517e0ade02ae92a8dda9803b5534bbb3d9
Amir9eng <amirmukhtarme@gmail.com>
2020-08-29 14:56:29 +0100

    feat: audio

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const alphabetsEl = document.querySelectorAll('img')
-
-function sounds (e) {
-  let key
+const alphabetsEl = document.querySelectorAll('img');
+alphabetsEl.forEach(alphabet =>{
+    alphabet.addEventListener('click',e=>{
+      let key=e.target.dataset.alphabet
+      const audio= document.querySelector(`audio[data-alphabet="${key}"]`);
+      audio.play();
+      
+         
+    })
+})
+const sounds = (e) =>{
   if(e.type === 'click') key=e.target.dataset.alphabet
 
   if(!key) return
   
-  const audio= document.querySelector(`audio[data-alphabet="${key}"]`)
 
   if(!audio) return
+
   window.addEventListener('click', playSound)
 }
